047a9d578b62e30c4e76d2cf51a2a03a1e8baef0
Andrew Rivera <drewisatlas@gmail.com>
2018-05-14 22:17:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,33 +1,25 @@
 var musicians = ['John Lennon', 'Paul McCartney', 'George Harrison', 'Ringo Starr'];
 var instruments= ['Guitar', 'Bass Guitar', 'Lead Guitar', 'Drums'];
+const facts =['He was the last Beatle to learn to drive', 'He was never a vegetarian', 'He was a choir boy and boy scout', 'He hated the sound of his own voice'];
+
 
 function theBeatlesPlay(musicians, instruments) {
-  var lineup = [];
-  for (let musiciansIndex = 0; musiciansIndex < musicians.length; musiciansIndex++) {
-    lineup.push(`${musicians[musiciansIndex]} plays ${instruments[musiciansIndex]}`);
+  let lineUp = [];
+  for (let i = 0; i < musicians.length; i++) {
+  lineUp.push(`${musicians[i]} plays ${instruments[i]}`);
   }
-  return lineup;
-}
+  return;
+  }
 
-
-
-const facts = ['He was the last Beatle to learn to drive', 'He was never a vegetarian', 'He was a choir boy and boy scout', 'He hated the sound of his own voice'];
-
-function johnLennonFacts(facts) {
- var excitingFacts = [];
-  while (facts.length > 0) {
-    excitingFacts.push(`${facts.shift()}!!!`);
-}
-return excitingFacts
+function johnLennonFacts(fact) {
+  let excitingFacts = [];
+  while (excitingFacts.length <= facts.length) {
+    excitingFacts.push(`${fact[0]} !!!`)
+  }
+  return;
 }
 
 function iLoveTheBeatles(number) {
-  var beatleLove = [];
-  do {
-    beatleLove.push('I love the Beatles!');
-    number++;
-  } while(number < 15);
+  let beatleLove = [];
   
-  return beatleLove
 }
-
